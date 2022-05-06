6c7285e663334bfff979acb06944f2b9bc00d83a
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-05-06 12:37:38 -0500

    updated movies-create-script.sql for exercise reference

--- movies-create-script.sql
+++ movies-create-script.sql
@@ -7,6 +7,10 @@ create database if not exists movies_db;
 
 use movies_db;
 
+drop table if exists movie_genre;
+drop table if exists movie_actor;
+drop table if exists actors;
+drop table if exists genres;
 drop table if exists movies; #drop the table(s) to which no other tables are dependent
 drop table if exists directors;
 
@@ -37,4 +41,40 @@ create table if not exists movies
 describe movies;
 
 
+create table if not exists genres
+(
+    id   int unsigned not null auto_increment primary key,
+    name varchar(120)
+);
 
+describe genres;
+
+
+create table if not exists movie_genre
+(
+    movie_id int unsigned not null,
+    genre_id int unsigned not null,
+    foreign key (movie_id) references movies(id),
+    foreign key (genre_id) references genres(id)
+
+);
+
+describe movie_genre;
+
+create table if not exists actors
+(
+    id int unsigned not null auto_increment primary key,
+    name varchar(255)
+);
+
+describe actors;
+
+create table if not exists movie_actor
+(
+    movie_id int unsigned not null,
+    actor_id int unsigned not null,
+    foreign key (movie_id) references movies (id),
+    foreign key (actor_id) references actors (id)
+);
+
+describe movie_actor;
